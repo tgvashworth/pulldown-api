0cef1b30db9da8b1692c12701cfc4be0a81bb953
Tom Ashworth <tom@phuu.net>
2013-05-05 11:05:18 +0100

    Test the API.

---
+++ test/test.js
@@ -0,0 +1,84 @@
+var request = require('request'),
+    url = require('url'),
+    t = require('tap'),
+    deepEqual = require('deeper');
+
+/**
+ * Configuration
+ */
+
+// Grab the config file if it's there
+var configFile;
+try {
+  configFile = require('../config.json');
+} catch (e) {
+  configFile = {};
+}
+
+// Then configure!
+var config = {};
+config.port = parseInt(process.argv[2], 10) ||
+              parseInt(process.env.PORT, 10) ||
+              configFile.port ||
+              9005;
+config.base = url.format({
+  protocol: 'http',
+  hostname: 'localhost',
+  port: config.port
+});
+
+/**
+ * Pulldown Packages
+ */
+
+// Grab the config file if it's there
+var registry;
+try {
+  registry = require('../pulldown.json');
+} catch (e) {
+  registry = {};
+}
+
+/**
+ * Tests
+ */
+
+t.test('registry', function (t) {
+  request(config.base, function (err, response, body) {
+    t.notOk(err);
+    t.ok(body);
+    var res = JSON.parse(body);
+    t.ok(deepEqual(registry, res));
+    t.end();
+  });
+});
+
+t.test('canonical', function (t) {
+  request(config.base + '/set/html5shiv', function (err, response, body) {
+    t.notOk(err);
+    t.ok(body);
+    var res = JSON.parse(body);
+    t.ok(deepEqual([registry['html5shiv']], res));
+    t.end();
+  });
+});
+
+t.test('alias', function (t) {
+  request(config.base + '/set/underscore', function (err, response, body) {
+    t.notOk(err);
+    t.ok(body);
+    var res = JSON.parse(body);
+    t.ok(deepEqual([registry['underscore']], res));
+    t.end();
+  });
+});
+
+t.test('set', function (t) {
+  request(config.base + '/set/backbone', function (err, response, body) {
+    t.notOk(err);
+    t.ok(body);
+    var res = JSON.parse(body);
+    t.ok(deepEqual(registry['backbone'], res));
+    t.end();
+  });
+});
